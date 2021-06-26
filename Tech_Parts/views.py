36b3fa359e9c404905ff15724c7360feef82804f
abdallah-nasir <abdallah.nasir@ymail.com>
2021-06-26 21:09:26 +0200

    deploy-21

--- Tech_Parts/views.py
+++ Tech_Parts/views.py
@@ -15,9 +15,9 @@ import json
 
 
 def home(request):
-    a=Type.objects.all()
-    for i in a:
-        Type.objects.create(name=i.name+"-copy",li=i.li)
+    # a=Type.objects.all()
+    # for i in a:
+    #     Type.objects.create(name=i.name+"-copy",li=i.li)
     type=Type.objects.all().order_by("id")
     form=TypeFilter(request.POST or None)
     if form.is_valid():
